d9e543c9a2e61e90a409e3837b20149499eafe8f
Nithya72 <nithya.anbalagan2@gmail.com>
2020-06-14 01:50:04 -0700

    Thumbnails & SafetyRating changes

--- app.py
+++ app.py
@@ -3,11 +3,30 @@ from rating import ratingLogic
 from rating import storesList
 app = Flask(__name__)
 
+@app.route("/rating")
+def rating():
+    return render_template("rating.html")
+
+@app.route("/safetyratings", methods=['POST'])
+def safetyratings():
+    dist = request.form['dist']
+    sanitize = request.form['sanitize']
+    avail = request.form['avail']
+    precaution = request.form['precaution']
+    density = request.form['density']
+    specialhr = request.form['specialhr']
+    aisle = request.form['aisle']
+
+    ratingform = [["dist", dist], ["sanitize",sanitize], ["avail",avail], ["precaution",precaution], ["density",density], ["specialhr",specialhr], ["aisle",aisle]]
+
+    stores = ratingLogic.processRatings(ratingform)
+    return render_template("index.html", title='Home', noHomePageLink=True)
+
 
 @app.route("/storesList/<category>")
 def stores_list(category):
-    catString = storesList.storesListings(category)
-    return render_template("stores.html", title="Stores List", catString=catString)
+    stores = storesList.storesListings(category)
+    return render_template("stores.html", title="Stores List", stores=stores)
 
 @app.route("/")
 def home():
--- rating/storesList.py
+++ rating/storesList.py
@@ -1,20 +1,11 @@
 import pandas as pd
 
 def storesListings(storeCategory):
-    # print(category)
     allStores = pd.read_csv('yelp_stores.csv')
-    print(allStores.head(10))
+    df = allStores[allStores['category'] ==storeCategory]
 
-    # storesBasedOnCategory = []
-    #
-    # for row in allStores:
-    #     if allStores['category'] == storeCategory:
-    #         storesBasedOnCategory.append(row)
-    #
-    # for i in storesBasedOnCategory:
-    #     print(i)
-
-
-    return storeCategory
+    stores = df.values
+    print(len(stores))
+    return stores
 
 # main()
