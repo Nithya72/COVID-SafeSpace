171615a36f5e71a8126c1a9fbe8242c73bcffa70
Nithya72 <60639346+Nithya72@users.noreply.github.com>
2020-06-14 22:04:33 -0700

    updated with rating logic

--- app.py
+++ app.py
@@ -27,7 +27,9 @@ def safetyratings():
     ess_avail = request.form['ess_avail']
     density_control = request.form['density_control']
     overall_rating = request.form['overall_rating']
-    ratingform = [["ft_dist", ft_dist], ["cart_sanitize",cart_sanitize], ["gm_precautions",gm_precautions], ["ess_avail",ess_avail], ["density_control",density_control], ["overall_rating",overall_rating]]
+    transport_mode = request.form['transport_mode']
+
+    ratingform = [["ft_dist", ft_dist], ["cart_sanitize",cart_sanitize], ["gm_precautions",gm_precautions], ["ess_avail",ess_avail], ["density_control",density_control], ["overall_rating",overall_rating], ["transport_mode",transport_mode]]
 
     print(ratingform)
 
@@ -36,15 +38,42 @@ def safetyratings():
         if overall_rating > 5:
             overall_rating = 5
 
-    print("store_id",store_id)
+    print("store_id", store_id)
 
-    insert_sql = "INSERT INTO ratings (store_id, ft_dist, cart_sanitize, gm_precautions, ess_avail, density_control, overall_rating) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute(insert_sql, (store_id, ft_dist, cart_sanitize, gm_precautions, ess_avail, density_control, overall_rating))
-    cursor.close()
-    conn.commit()
-    conn.close()
+    try:
+        insert_sql = "INSERT INTO ratings (store_id, ft_dist, cart_sanitize, gm_precautions, ess_avail, density_control, overall_rating) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute(insert_sql, (store_id, ft_dist, cart_sanitize, gm_precautions, ess_avail, density_control, overall_rating))
+        conn.commit()
+
+        select_sql = "SELECT * FROM Ratings WHERE store_id = %s ORDER BY rating_id DESC"
+        cursor.execute(select_sql, store_id)
+
+        # Fetching latest 5 records to re-calulate rating average
+        rating_data = cursor.fetchmany(5)
+        rating = 0
+        count = 0
+        for row in rating_data:
+            print(row)
+            rating = rating+row[2]
+            count=count+1
+
+        avg_rating = rating/count
+
+        print("avg_rating", avg_rating)
+        print("rating", rating)
+        print("count", count)
+
+        update_sql = "UPDATE Stores SET star_rating= %s WHERE store_id = %s"
+        cursor.execute(update_sql, (avg_rating, store_id))
+
+    except Exception as ex:
+        print("Problem Executing SQL queries: " + str(ex))
+    finally:
+        cursor.close()
+        conn.commit()
+        conn.close()
 
     return redirect(url_for('stores_list', category=category))
 
